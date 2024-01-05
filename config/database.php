087d24689272fe2ef6423795f0d3379230cc3dca
Jeronimo <jeropauli@hotmail.com>
2024-01-04 19:03:56 -0500

    Database connection

---
+++ config/database.php
@@ -0,0 +1,35 @@
+<?php
+
+class Database{
+
+    private $hostname = "localhost";
+    private $database = "TERA_DB";
+    private $username = "root";
+    private $password = "minecraft99";
+    private $charset = "utf8";
+
+    function connect(){
+        try{
+        $connection = "mysql:host=" . $this->hostname . "; dbname=" . $this->database . ";
+        charset=" . $this->charset;
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_EMULATE_PREPARES => false
+        ];
+
+        $pdo = new PDO($connection, $this->username, $this->password, $options);
+
+        return $pdo;
+
+        }catch(PDOException $e){
+
+            echo 'Error conexion: ' . $e->getMessage();
+            exit;
+
+        }
+
+
+    }
+}
+
+?>
